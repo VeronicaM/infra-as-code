0ffde4a730e5c0db352b4345537143a29c93e152
Veronica Mihai <mihaiveronica@ymail.com>
2020-04-13 17:39:39 +0300

    add create and update stack scripts

---
+++ create-stack.sh
@@ -0,0 +1,4 @@
+stack_name=iaac_udacity_mv
+config_path=config.yml
+params_path=stack_params.json
+aws cloudformation create-stack --stack-name $stack_name --template-body file://$config_path  --parameters file://$params_path --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
