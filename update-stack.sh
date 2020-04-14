56d50629350f792d32ff9e1eaae3e0987cbeaaec
Veronica Mihai <mihaiveronica@ymail.com>
2020-04-14 09:30:13 +0300

    fix errors in create and update stack scripts + fix typo for role name

--- update-stack.sh
+++ update-stack.sh
@@ -1,4 +1,4 @@
-stack_name=iaac_udacity_mv
-config_path=config.yml
-params_path=stack_params.json
-aws cloudformation update-stack --stack-name $stack_name --template-body file://$config_path  --parameters file://$params_path --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
+stack_name=iaac-udacity-mv
+config_path=file://config.yml
+params_path=file://stack-params.json
+aws cloudformation update-stack --stack-name $stack_name --template-body $config_path  --parameters $params_path --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
